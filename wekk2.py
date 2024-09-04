4a06c3700a82bc99d9efa8f324529b674fedc1a5
Zarco <jzarco4@cps.k12.il.us>
2024-09-04 14:59:31 -0500

    first commit

---
+++ wekk2.py
@@ -0,0 +1,9 @@
+#ask someone what day it is
+#ask them for their favorite holiday
+#print out the day and their favorite holiday
+#a sentence
+#making a change to the file
+day = input("what day is it")
+print("today is" + day)
+holiday = input("what is your favorite holiday")
+print( holiday + "is also my favorite holiday")
